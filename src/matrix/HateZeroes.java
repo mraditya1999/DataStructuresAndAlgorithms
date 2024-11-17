9b176f6eae31fb7edfd516a9a832767c12fe9a38
Aditya Yadav <ay5480620@gmail.com>
2024-11-17 08:08:23 +0530

    isPlaindromic string

--- src/matrix/HateZeroes.java
+++ src/matrix/HateZeroes.java
@@ -2,23 +2,29 @@ package matrix;
 
 public class HateZeroes {
     public static void main(String[] args) {
-        int[][] arr = {{1, 0, 2, 2}, {1, 0, 3, 4}, {0, 0, 0, 0}, {5, 0, 6, 7}};
-        int cnt = 0;
+        int[][] arr = {{1, 0, 2, 2}, {1, 0, 3, 4}, {0, 0, 0, 0}, {5, 0, 6, 7}, {6, 0, 7, 8}};
+        int rowCount = 0;
+        int colCount = 0;
 
         System.out.println("Before: ");
         display(arr);
 
         for (int i = 0; i < arr.length; i++) {
+            colCount = 0;
             for (int j = 0; j < arr[0].length; j++) {
-                if (arr[i][j] != 0) cnt++;
+                if (arr[i][j] != 0) colCount++;
             }
-            break;
+            if (arr[i][0] != 0) {
+                rowCount++;
+            }
         }
 
-        int[][] newArr = new int[cnt][cnt];
+        System.out.println("Row: " + rowCount);
+        System.out.println("Col: " + colCount);
+        int[][] newArr = new int[rowCount][colCount];
 
         for (int i = 0, m = 0; i < arr.length; i++) {
-            for (int j = 0, n = 0; j < arr.length; j++) {
+            for (int j = 0, n = 0; j < arr[0].length; j++) {
                 if (arr[i][j] != 0) {
                     newArr[m][n] = arr[i][j];
                     n++;
@@ -34,10 +40,11 @@ public class HateZeroes {
 
     public static void display(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+            for (int j = 0; j < arr[0].length; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
     }
 }
+
