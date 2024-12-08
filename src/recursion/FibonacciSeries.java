abd8ff1065c708ce6c3fdf9429511522d45f65eb
Aditya Yadav <ay5480620@gmail.com>
2024-12-08 07:01:15 +0530

    add fibonacci series using recursion

--- src/recursion/FibonacciSeries.java
+++ src/recursion/FibonacciSeries.java
@@ -1,30 +1,37 @@
-//package recursion;
-//
-//import java.util.Scanner;
-//
-//public class FibonacciSeries {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter the number : ");
-//        int N = sc.nextInt();
-//
-//        displayFibonacciSeries(N);
-//    }
-//
-//    public static void displayFibonacciSeries(int n) {
-//        if (n == 0 || n == 1) {
-//            System.out.println(n);
-//            return;
-//        }
-//
-//
-//    }
-//
-//    public static int sumOfFibonacciSeries(int n) {
-//        if (n == 0 || n == 1) {
-//            return n;
-//        }
-//
-//
-//    }
-//}
+package recursion;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int N = sc.nextInt();
+        sc.close();
+
+        // Print the Nth Fibonacci term
+        int nthTerm = findNthFibonacci(N);
+        System.out.println("Nth Fibonacci term is: " + nthTerm);
+
+        // Print the entire Fibonacci series up to N terms
+        System.out.print("Fibonacci series up to N terms: ");
+        for (int i = 1; i <= N; i++) {
+            System.out.print(findNthFibonacci(i) + " ");
+        }
+        System.out.println();
+
+        // Print the sum of the entire Fibonacci series up to N terms
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            sum += findNthFibonacci(i);
+        }
+        System.out.println("Sum of Fibonacci series up to N terms: " + sum);
+    }
+
+    public static int findNthFibonacci(int N) {
+        if (N == 1 || N == 2) {
+            return N - 1;
+        }
+        return findNthFibonacci(N - 1) + findNthFibonacci(N - 2);
+    }
+}
